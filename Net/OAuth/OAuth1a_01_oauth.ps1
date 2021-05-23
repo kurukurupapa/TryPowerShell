0573af905bc84ac8631fa0fa62746b441dfa6db2
kurukurupapa <gootarapapa@gmail.com>
2021-05-23 15:58:38 +0900

    Update

--- Net/OAuth/OAuth1a_01_oauth.ps1
+++ Net/OAuth/OAuth1a_01_oauth.ps1
@@ -1,5 +1,8 @@
 # PowerShell標準機能で OAuth 1.0a クライアントを作成してみる
 # ・アクセストークンの有効制限や再取得を考慮していません。
+# 参考
+# [OAuth Core 1.0a](https://oauth.net/core/1.0a/#sig_base_example)
+# [curl - Powershell OAuth 1.0 'one-legged' authentication with HMAC-SHA1 fails - Stack Overflow](https://stackoverflow.com/questions/60992276/powershell-oauth-1-0-one-legged-authentication-with-hmac-sha1-fails)
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
@@ -7,76 +10,80 @@ $VerbosePreference = 'Continue'
 $DebugPreference = 'SilentlyContinue'
 
 # Signature Base String を取得
-# [OAuth Core 1.0a](https://oauth.net/core/1.0a/#sig_base_example)
-# [curl - Powershell OAuth 1.0 'one-legged' authentication with HMAC-SHA1 fails - Stack Overflow](https://stackoverflow.com/questions/60992276/powershell-oauth-1-0-one-legged-authentication-with-hmac-sha1-fails)
-# メソッドは、大文字であること。
 function GetSignatureBaseString($method, $url, $params) {
   $arr = $params.GetEnumerator() | sort Name | %{
-    $_.Name + '=' + $_.Value
+    # $_.Name + '=' + $_.Value
+    [Uri]::EscapeDataString($_.Name) + '=' + [Uri]::EscapeDataString($_.Value)
   }
-  $signatureBaseString = $method.ToUpper() + '&' + [Uri]::EscapeDataString($url) + '&' + [Uri]::EscapeDataString($arr -join '&')
-  Write-Debug "SignatureBaseString: $signatureBaseString"
+  $signatureBaseString = $method.ToUpper() + '&' + [Uri]::EscapeDataString($url.ToLower()) + '&' + [Uri]::EscapeDataString($arr -join '&')
+  Write-Debug ("SignatureBaseString: " + $signatureBaseString -replace ('&',"&`n  ") -replace ('%26',"%26`n  "))
   return $signatureBaseString
 }
-# GetSignatureBaseString "POST" "https://sample.com" @{a="1";b="2"}
+# GetSignatureBaseString "POST" "https://sample.com" @{a="1";b="2";c="A B"}
 
 # oauth_signatureを取得
 function GetSignature($signatureBaseString, $consumerKey, $tokenSecret) {
-  $key = $consumerSecret + '&' + $tokenSecret
+  # $key = $consumerSecret + '&' + $tokenSecret
+  $key = [Uri]::EscapeDataString($consumerSecret) + '&' + [Uri]::EscapeDataString($tokenSecret)
   $hmacsha1 = New-Object System.Security.Cryptography.HMACSHA1
   $hmacsha1.Key = [System.Text.Encoding]::ASCII.GetBytes($key)
   $signature = [System.Convert]::ToBase64String($hmacsha1.ComputeHash(
     [System.Text.Encoding]::ASCII.GetBytes($signatureBaseString)))
-  Write-Debug "Key: $key"
-  Write-Debug "Signature: $signature"
+  Write-Debug "Signature: $signature, Key: $key"
   return $signature
 }
 # GetSignature "abc" "def" "ghi"
 
 # Authorizationヘッダーを取得
 function GetAuthorizationHeader($params, $signature) {
-  $params2 = $params.Clone()
-  $params2['oauth_signature'] = $signature
-  $arr = $params2.GetEnumerator() | sort Name | %{
+  $params['oauth_signature'] = $signature
+  $arr = $params.GetEnumerator() | sort Name | %{
     [Uri]::EscapeDataString($_.Name) + '="' + [Uri]::EscapeDataString($_.Value) + '"'
   }
   $result = "OAuth " + ($arr -join ",")
-  Write-Debug "Authorization: $result"
+  Write-Debug ("Authorization header: " + $result -replace (',',",`n  "))
   return $result
 }
 # GetAuthorization @{a="1";b="2"} "abc"
 
 # レスポンスを解析
-function ParseResponse($responseStr) {
+function ParseOauthResponse($responseStr) {
   Write-Debug "Response: $responseStr"
   $hash = @{}
   $responseStr.Split('&') | %{
     $name, $value = $_.Split('=')
-    $hash[$name] = $value
+    $hash[[Uri]::UnescapeDataString($name)] = [Uri]::UnescapeDataString($value)
   }
   return $hash
 }
-# ParseResponse "oauth_token=aaa&oauth_token_secret=bbb&oauth_callback_confirmed=true"
+# ParseOauthResponse "oauth_token=aaa&oauth_token_secret=bbb&oauth_callback_confirmed=true"
 
 # HTTPアクセスエラー時のエラー情報を表示
 function PrintWebException($e) {
-  Write-Host $e
-  Write-Host "$($e.Exception.Status.value__) $($e.Exception.Status.ToString())"
-  $stream = $e.Exception.Response.GetResponseStream()
-  $reader = New-Object System.IO.StreamReader $stream
-  $reader.BaseStream.Position = 0
-  $reader.DiscardBufferedData()
-  Write-Host $reader.ReadToEnd()
-  $stream.Close()
+  try {
+    Write-Host $e
+    Write-Host "$($e.Exception.Status.value__) $($e.Exception.Status.ToString())"
+    $stream = $e.Exception.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader $stream
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    Write-Host $reader.ReadToEnd()
+    $stream.Close()
+  } catch {
+    Write-Host "PrintWebException ERROR: $_"
+  }
 }
 
 # OAuth1.0aでAPI呼び出しを実行
-# 引数の $token, $tokenSecret, $oauthVerifier, $optionParams は、必要に応じて設定する。
-# リクエストトークン取得時は、$oauthToken, $oauthTokenSecret, $oauthVerifier なし。
-# アクセストークン取得時は、$oauthToken, $oauthTokenSecret にリクエストトークン取得結果を設定し、$oauthVerifier にユーザ認証結果を設定する。
-# リソースAPI呼び出し時は、$oauthToken, $oauthTokenSecret にアクセストークン取得結果を設定し、呼び出すAPIにGET/POSTパラメータを付加するときは、optionParams も設定する。
-function InvokeOauthApi($method, $url, $callbackUrl, $consumerKey, $consumerSecret,
-  $oauthToken='', $oauthTokenSecret='', $oauthVerifier=$null, $optionParams=@{}) {
+# 引数の $callback, $token, $tokenSecret, $verifier, $optionParams, $body は、必要に応じて設定する。
+# $optionParamsの値は、Authorizationヘッダーやoauth_signatureの計算に含める。
+# $bodyの値は、GETパラメータまたはPOSTデータとなる。
+# リクエストトークン取得時は、$token, $tokenSecret, $verifier なし、必要に応じて $callback を設定する。
+# アクセストークン取得時は、$token, $tokenSecret にリクエストトークン取得結果を設定し、$verifier にユーザ認証結果を設定する。
+# リソースAPI呼び出し時は、$token, $tokenSecret にアクセストークン取得結果を設定し、呼び出すAPIにGET/POSTパラメータを付加するときは、optionParams を設定する。
+function InvokeOauthApi($method, $url, $consumerKey, $consumerSecret,
+  $callback=$null, $token='', $tokenSecret='', $verifier=$null, $optionParams=@{}, $body=$null,
+  $signatureMethod='HMAC-SHA1') {
 
   # oauth_nonceは、一意な値であればよいので、とりあえずタイムスタンプから作成する。
   $timestamp = [int]((Get-Date) - [Datetime]"1970/1/1 00:00:00 GMT").TotalSeconds
@@ -85,38 +92,49 @@ function InvokeOauthApi($method, $url, $callbackUrl, $consumerKey, $consumerSecr
   $params = @{
     "oauth_consumer_key" = $consumerKey
     "oauth_nonce" = $nonce
-    "oauth_signature_method" = "HMAC-SHA1"
+    "oauth_signature_method" = $signatureMethod
     "oauth_timestamp" = $timestamp
-    "oauth_token" = $oauthToken
     "oauth_version" = "1.0"
   }
-  if ($callbackUrl) {
-    $params['oauth_callback'] = $callbackUrl
+  if ($token) {
+    $params["oauth_token"] = $token
   }
-  if ($oauthVerifier) {
-    $params['oauth_verifier'] = $oauthVerifier
+  if ($verifier) {
+    $params['oauth_verifier'] = $verifier
+  }
+  if ($callback) {
+    $params['oauth_callback'] = $callback
   }
   if ($optionParams) {
     $params += $optionParams
   }
   $signatureBaseString = GetSignatureBaseString $method $url $params
-  $signature = GetSignature $signatureBaseString $consumerSecret $oauthTokenSecret
+  $signature = GetSignature $signatureBaseString $consumerSecret $tokenSecret
   $authorizationHeader = GetAuthorizationHeader $params $signature
 
   try {
     $headers = @{
       Authorization = $authorizationHeader
+      'User-Agent' = "PsOauth1aLocalClient"
     }
-    $res = Invoke-RestMethod $url -Method $method -Headers $headers -Body $optionParams
-    return $res
+    if ($method -eq 'POST') {
+      $headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    }
+    return Invoke-RestMethod $url -Method $method -Headers $headers -Body $body
   } catch {
     PrintWebException $_
     throw $_
   }
 }
 
-# OAuth認証
-# 事前に $consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl, $callbackUrl を設定しておく
+# ユーザ認可URLの実行（ブラウザが開く）
+function InvokeUserAuthorization($token) {
+  $url = $authUrl + "?oauth_token=" + [System.Web.HttpUtility]::UrlEncode($token)
+  Write-Verbose $url
+  Start-Process $url
+}
+
+# OAuth1.0a認証のリクエストトークンからアクセストークン取得までのフロー
 function InvokeOauthFlow($consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl) {
   # １．リクエストトークン取得
   # [OAuth Core 1.0a](https://oauth.net/core/1.0a/#auth_step1) 6.1. Obtaining an Unauthorized Request Token
@@ -125,32 +143,43 @@ function InvokeOauthFlow($consumerKey, $consumerSecret, $requestUrl, $authUrl, $
   # リクエストトークン取得処理では、oauth_callbackに"oob"(out-of-band)を設定しなければならない模様。
   # OAuth 1.0a の仕様としては、oauth_* 以外の追加パラメータがあるときだけ、POSTデータ/GETパラメータを設定するように見える。今回追加パラメータなしなので関係ないけど。
   Write-Verbose "リクエストトークン取得 開始"
-  $res = InvokeOauthApi 'POST' $requestUrl 'oob' $consumerKey $consumerSecret
-  $res = ParseResponse $res
+  $res = ParseOauthResponse(
+    InvokeOauthApi 'POST' $requestUrl $consumerKey $consumerSecret -callback 'oob')
 
   # ２．ユーザによる承認
   # [OAuth Core 1.0a](https://oauth.net/core/1.0a/#auth_step2) 6.2. Obtaining User Authorization
   # [GET oauth/authorize | Twitter Developer](https://developer.twitter.com/en/docs/authentication/api-reference/authorize)
   Write-Verbose "ユーザ認証 開始"
-  $url = $authUrl + "?oauth_token=" + $res.oauth_token
-  Write-Verbose $url
-  Start-Process $url
-  $oauthVerifier = Read-Host "完了画面に表示されたトークンを入力してください。"
+  InvokeUserAuthorization $res.oauth_token
+  $verifier = Read-Host "完了画面に表示されたトークンを入力してください。"
 
   # ３．アクセストークン取得
   # [OAuth Core 1.0a](https://oauth.net/core/1.0a/#auth_step3) 6.3. Obtaining an Access Token
   # [POST oauth/access_token | Twitter Developer](https://developer.twitter.com/en/docs/authentication/api-reference/access_token)
   # アクセストークン取得処理では、oauth_callbackに"oob"(out-of-band)を設定するか、oauth_callback自体を送信しない模様。
   Write-Verbose "アクセストークン取得 開始"
-  $res = InvokeOauthApi 'POST' $accessUrl $null $consumerKey $consumerSecret $res.oauth_token $res.oauth_token_secret $oauthVerifier
-  return ParseResponse $res
+  return ParseOauthResponse(
+    InvokeOauthApi 'POST' $accessUrl $consumerKey $consumerSecret `
+    -token $res.oauth_token -tokenSecret $res.oauth_token_secret -verifier $verifier)
 }
 # $DebugPreference = 'Continue'
 # $token = InvokeOauthFlow $consumerKey $consumerSecret $requestUrl $authUrl $accessUrl
 
+# 簡易的にデータを暗号化ファイルとして保存/読み込みできるようにした。（あまり良くない実装かもしれない）
+function SaveSecretObject($path, $obj) {
+  $jsonStr = ConvertTo-Json $obj -Compress
+  ConvertTo-SecureString $jsonStr -AsPlainText -Force | ConvertFrom-SecureString | Set-Content $path
+}
+function LoadSecretObject($path) {
+  $ss = Get-Content $path | ConvertTo-SecureString
+  $jsonStr = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR(
+    [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ss))
+  return ConvertFrom-Json $jsonStr
+}
+
 # クラス化
 # 簡易的にデータを暗号化ファイルとして保存/読み込みできるようにした。（あまり良くない実装かもしれない）
-class OAuth1aLocalClient {
+class Oauth1aLocalClient {
   $consumerKey
   $consumerSecret
   $requestUrl
@@ -159,9 +188,9 @@ class OAuth1aLocalClient {
   $accessToken
   $accessTokenSecret
 
-  OAuth1aLocalClient() {
+  Oauth1aLocalClient() {
   }
-  OAuth1aLocalClient($consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl) {
+  Oauth1aLocalClient($consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl) {
     $this.consumerKey = $consumerKey
     $this.consumerSecret = $consumerSecret
     $this.requestUrl = $requestUrl
@@ -178,28 +207,27 @@ class OAuth1aLocalClient {
   [object] Invoke($method, $url) {
     return $this.Invoke($method, $url, $null)
   }
-  [object] Invoke($method, $url, $optionParams) {
+  [object] Invoke($method, $url, $optionParams, $body) {
     if (!$this.accessToken -or !$this.accessTokenSecret) {
       $this.InvokeOauthFlow()
     }
-    return InvokeOauthApi $method $url $null $this.consumerKey $this.consumerSecret $this.accessToken $this.accessTokenSecret -optionParams $optionParams
+    return InvokeOauthApi $method $url $this.consumerKey $this.consumerSecret `
+      -token $this.accessToken -tokenSecret $this.accessTokenSecret -optionParams $optionParams -body $body
   }
 
   Save($path) {
-    $jsonStr = ConvertTo-Json $this -Compress
-    ConvertTo-SecureString $jsonStr -AsPlainText -Force | ConvertFrom-SecureString | Set-Content $path
+    SaveSecretObject $path $this
   }
 
   Load($path) {
-    $ss = Get-Content $path | ConvertTo-SecureString
-    $jsonStr = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR(
-      [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ss))
-    (ConvertFrom-Json $jsonStr).psobject.properties | %{
+    $data = LoadSecretObject $path
+    $data.psobject.properties | %{
       $this.($_.Name) = $_.Value
     }
   }
 }
-# $client = New-Object OAuth1aLocalClient
-# $path = Join-Path $home "PSOAuth1aLocalClient_Dummy.dat"
+# $path = Join-Path $home "PsOauth1aLocalClient_Dummy.dat"
+# $client = New-Object Oauth1aLocalClient
+# $client.InvokeOauthFlow()
 # $client.Save($path)
 # $client.Load($path)
--- Net/OAuth/OAuth1a_01_zaim.ps1
+++ Net/OAuth/OAuth1a_01_zaim.ps1
@@ -1,5 +1,4 @@
 # PowerShell標準機能で OAuth 1.0a クライアントを作成してみる
-
 # Zaim API で動作確認する。
 # ZaimAPIへのアプリ登録では、サービス種類を「クライアントアプリ」、アクセスレベルを読み込みのみにした。
 # でも、サービス種類を「ブラウザアプリ」にしても動作した。
@@ -9,27 +8,91 @@ $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 $DebugPreference = 'SilentlyContinue'
 
-$oauthPath = Join-Path $home "PSOAuth1aLocalClient_Zaim.dat"
-if (Test-Path $oauthPath) {
-  $client = New-Object OAuth1aLocalClient
-  $client.Load($oauthPath)
+# OAuthフローを1ステップずつ確認
+# 事前に、$consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl を設定しておく。
+$DebugPreference = 'Continue'
+
+# リクエストトークン
+ParseOauthResponse(InvokeOauthApi 'POST' $requestUrl $consumerKey $consumerSecret -callback 'oob') | Tee-Object -Variable res
+#=> oauth_token=xxx&oauth_token_secret=xxx&oauth_callback_confirmed=true
+
+# ユーザ認可
+InvokeUserAuthorization $res.oauth_token
+$verifier = Read-Host "完了画面に表示されたトークンを入力してください。"
+
+# アクセストークン
+ParseOauthResponse(InvokeOauthApi 'POST' $accessUrl $consumerKey $consumerSecret -token $res.oauth_token -tokenSecret $res.oauth_token_secret -verifier $verifier) | Tee-Object -Variable res
+#=> oauth_token=xxx&oauth_token_secret=xxx
+
+# 保存
+$dataPath = Join-Path $home "PsOauth1aLocalClient_Zaim.dat"
+$data = @{
+  consumerKey = $consumerKey
+  consumerSecret = $consumerSecret
+  requestUrl = $requestUrl
+  authUrl = $authUrl
+  accessUrl = $accessUrl
+  accessToken = $res.oauth_token
+  accessTokenSecret = $res.oauth_token_secret
+}
+SaveSecretObject $dataPath $data
+LoadSecretObject $dataPath
+
+# リソースAPI
+InvokeOauthApi 'GET' 'https://api.zaim.net/v2/home/user/verify' $consumerKey $consumerSecret -token $data.accessToken -tokenSecret $data.accessTokenSecret | ConvertTo-Json -Depth 100
+InvokeOauthApi 'GET' 'https://api.zaim.net/v2/home/account' $consumerKey $consumerSecret -token $data.accessToken -tokenSecret $data.accessTokenSecret -optionParams @{ mapping = 1 } | ConvertTo-Json -Depth 100
+InvokeOauthApi 'GET' 'https://api.zaim.net/v2/home/category' $consumerKey $consumerSecret -token $data.accessToken -tokenSecret $data.accessTokenSecret -optionParams @{ mapping = 1 } | ConvertTo-Json -Depth 100
+InvokeOauthApi 'GET' 'https://api.zaim.net/v2/home/genre' $consumerKey $consumerSecret -token $data.accessToken -tokenSecret $data.accessTokenSecret -optionParams @{ mapping = 1 } | ConvertTo-Json -Depth 100
+$params = @{
+  mapping = 1
+  start_date = (Get-Date).AddDays(-7).ToString("yyyy-MM-dd")
+  end_date = Get-Date -Format "yyyy-MM-dd"
+}
+InvokeOauthApi 'GET' 'https://api.zaim.net/v2/home/money' $consumerKey $consumerSecret -token $data.accessToken -tokenSecret $data.accessTokenSecret $params -body $params | ConvertTo-Json -Depth 100
+$params = @{
+  mapping = 1
+  category_id = 19
+  date = Get-Date -Format "yyyy-MM-dd"
+  amount = 1000000
+  to_account_id = 1
+  place = "Dummy ダミー ショップ"
+  comment = "OAuth 1.0a Client Test `"#$%&+,-./:;<=>?@[\]^_`{|}~"
+  # comment = "OAuth 1.0a Client Test !`"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  # NG記号 !'()*
+}
+InvokeOauthApi 'POST' 'https://api.zaim.net/v2/home/money/income' $consumerKey $consumerSecret -token $data.accessToken -tokenSecret $data.accessTokenSecret -optionParams $params -body $params | ConvertTo-Json -Depth 100
+
+# クラス版
+$dataPath = Join-Path $home "PsOauth1aLocalClient_Zaim.dat"
+if (Test-Path $dataPath) {
+  $client = New-Object Oauth1aLocalClient
+  $client.Load($dataPath)
 } else {
-  $client = New-Object OAuth1aLocalClient($consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl)
+  # 事前に $consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl, $callbackUrl を設定しておく
+  $client = New-Object Oauth1aLocalClient($consumerKey, $consumerSecret, $requestUrl, $authUrl, $accessUrl)
   $client.InvokeOauthFlow()
-  $client.Save($oauthPath)
+  $client.Save($dataPath)
 }
 
-$client.Invoke('GET', 'https://api.zaim.net/v2/home/user/verify') | ConvertTo-Json -Depth 100 | %{ Write-Host $_ }
-$client.Invoke('GET', 'https://api.zaim.net/v2/home/account', @{ mapping = 1 }) | ConvertTo-Json -Depth 100 | %{ Write-Host $_ }
-$client.Invoke('GET', 'https://api.zaim.net/v2/home/category', @{ mapping = 1 }) | ConvertTo-Json -Depth 100 | %{ Write-Host $_ }
-$client.Invoke('GET', 'https://api.zaim.net/v2/home/genre', @{ mapping = 1 }) | ConvertTo-Json -Depth 100 | %{ Write-Host $_ }
-$client.Invoke('GET', 'https://api.zaim.net/v2/home/money', @{
+$client.Invoke('GET', 'https://api.zaim.net/v2/home/user/verify') | ConvertTo-Json -Depth 100
+$client.Invoke('GET', 'https://api.zaim.net/v2/home/account', @{ mapping = 1 }) | ConvertTo-Json -Depth 100
+$client.Invoke('GET', 'https://api.zaim.net/v2/home/category', @{ mapping = 1 }) | ConvertTo-Json -Depth 100
+$client.Invoke('GET', 'https://api.zaim.net/v2/home/genre', @{ mapping = 1 }) | ConvertTo-Json -Depth 100
+$params = @{
   mapping = 1
   start_date = (Get-Date).AddDays(-7).ToString("yyyy-MM-dd")
   end_date = Get-Date -Format "yyyy-MM-dd"
-}) | ConvertTo-Json -Depth 100 | %{ Write-Host $_ }
-$client.Invoke('POST', 'https://api.zaim.net/v2/home/money', @{
+}
+$client.Invoke('GET', 'https://api.zaim.net/v2/home/money', $params, $params) | ConvertTo-Json -Depth 100
+$params = @{
   mapping = 1
-  start_date = (Get-Date).AddDays(-7).ToString("yyyy-MM-dd")
-  end_date = Get-Date -Format "yyyy-MM-dd"
-}) | ConvertTo-Json -Depth 100 | %{ Write-Host $_ }
+  category_id = 19
+  date = Get-Date -Format "yyyy-MM-dd"
+  amount = 1000000
+  to_account_id = 1
+  place = "Dummy ダミー ショップ"
+  comment = "OAuth 1.0a Client Test `"#$%&+,-./:;<=>?@[\]^_`{|}~"
+  # comment = "OAuth 1.0a Client Test !`"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  # NG記号 !'()*
+}
+$client.Invoke('POST', 'https://api.zaim.net/v2/home/money/income', $params, $params) | ConvertTo-Json -Depth 100
