210ba87977821f2238a26bd6ae1391c0766deb59
kurukurupapa <gootarapapa@gmail.com>
2022-05-19 22:11:01 +0900

    Add ScreenCapture

---
+++ Windows/ScreenCapture_Memo.ps1
@@ -0,0 +1,79 @@
+# 画面キャプチャを撮るPowerShellスクリプトの開発メモ
+
+# 動作確認
+# Visual Studio Code なら、実行したい行を選択して、F8キーで実行できる。
+
+# 画面キャプチャ
+Add-Type -AssemblyName System.Drawing
+function CaptureToFile($rect, $outPath) {
+  $bitmap = New-Object System.Drawing.Bitmap($rect.Width, $rect.Height)
+  $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+  $graphics.CopyFromScreen($rect.Left, $rect.Top, 0, 0, $bitmap.Size)
+  $bitmap.Save($outPath)
+  $graphics.Dispose()
+  $bitmap.Dispose()
+  Write-Host "キャプチャしました。 $($rect.Location) $($rect.Size) $outPath"
+}
+$rect = [System.Drawing.Rectangle]::FromLTRB(0, 0, 500, 500)
+$outPath = "work\a.png"
+CaptureToFile $rect $outPath
+
+function CaptureToClipboard($rect) {
+  $bitmap = New-Object System.Drawing.Bitmap($rect.Width, $rect.Height)
+  $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
+  $graphics.CopyFromScreen($rect.Left, $rect.Top, 0, 0, $bitmap.Size)
+  # Set-Clipboard -Value $bitmap
+  # Set-Clipboard -Value $graphics -Format Image
+  [Windows.Forms.Clipboard]::SetImage($bitmap)
+  $graphics.Dispose()
+  $bitmap.Dispose()
+  Write-Host "キャプチャしました。 $($rect.Location) $($rect.Size)"
+}
+CaptureToClipboard $rect
+Get-Clipboard -Format Image
+
+# 画像をクリップボードにコピー
+$bitmap = New-Object System.Drawing.Bitmap($outPath)
+# 案1
+$data = New-Object System.Windows.Forms.DataObject
+$data.SetImage($bitmap)
+[Windows.Forms.Clipboard]::SetDataObject($data, $true)
+# 案2
+[Windows.Forms.Clipboard]::SetImage($bitmap)
+# 
+Get-Clipboard -Format Image
+$bitmap.Dispose()
+
+# マウスの座標
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Cursor]::Position
+[System.Windows.Forms.Control]::MouseButtons
+[System.Windows.Forms.Control]::MousePosition
+
+function GetUserRect() {
+  Write-Host "マウスをドラッグして矩形領域を選択してください。"
+  while ([System.Windows.Forms.Control]::MouseButtons -eq 'None') { Start-Sleep 0.5 }; Write-Host "Pressed"
+  $p1 = [System.Windows.Forms.Control]::MousePosition
+  while ([System.Windows.Forms.Control]::MouseButtons -ne 'None') { Start-Sleep 0.5 }; Write-Host "Released"
+  $p2 = [System.Windows.Forms.Control]::MousePosition
+  $rect = [System.Drawing.Rectangle]::FromLTRB([Math]::Min($p1.X, $p2.X), [Math]::Min($p1.Y, $p2.Y), [Math]::Max($p1.X, $p2.X), [Math]::Max($p1.Y, $p2.Y))
+  Write-Host $rect
+  return $rect
+}
+$rect = GetUserRect
+
+# 参考サイト
+# [PowerShell モジュール ブラウザー - PowerShell | Microsoft Docs](https://docs.microsoft.com/ja-jp/powershell/module/?view=powershell-5.1)
+# [.NET API ブラウザー | Microsoft Docs](https://docs.microsoft.com/ja-jp/dotnet/api/?view=netframework-4.5)
+# [Bitmap クラス (System.Drawing) | Microsoft Docs](https://docs.microsoft.com/ja-jp/dotnet/api/system.drawing.bitmap?view=netframework-4.5)
+# [Clipboard クラス (System.Windows) | Microsoft Docs](https://docs.microsoft.com/ja-jp/dotnet/api/system.windows.clipboard?view=netframework-4.5)
+# [Graphics クラス (System.Drawing) | Microsoft Docs](https://docs.microsoft.com/ja-jp/dotnet/api/system.drawing.graphics?view=netframework-4.5)
+# [簡単！PowerShellで作成できる画面録画ツールの紹介と作り方[No18] - BookALittle](https://bookalittle.com/howtocheck-operation-byrecording-pstool/)
+# [インターネット上の画像をクリップボードにコピーするコード(PowerShell版)](https://gist.github.com/bu762/6e0f3668e59d4a932821)
+
+# 動作確認環境
+$PSVersionTable
+# PSVersion                      5.1.19041.1682
+# PSEdition                      Desktop
+Get-Item "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"
+# Version       : 4.8.04084
