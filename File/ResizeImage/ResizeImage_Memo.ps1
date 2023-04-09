80bd471ae09902bf2b2565b42adbdef47a62d310
kurukurupapa <gootarapapa@gmail.com>
2023-04-09 20:59:32 +0900

    Update File\ResizeImage

---
+++ File/ResizeImage/ResizeImage_Memo.ps1
@@ -0,0 +1,7 @@
+# ResizeImageコマンド開発メモ
+
+.\File\ResizeImage\ResizeImage.ps1
+$DebugPreference = 'Continue'; .\File\ResizeImage\ResizeImage.ps1 -Verbose D:\tmp\srcimage.jpg 300 200
+
+.\File\ResizeImage\ImageEditForm.ps1
+$DebugPreference = 'Continue'; .\File\ResizeImage\ImageEditForm.ps1 -Verbose D:\tmp\srcimage.jpg
