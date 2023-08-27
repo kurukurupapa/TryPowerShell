c16db8c31629aa9882ceccf81b7d489f56d8bf38
kurukurupapa <gootarapapa@gmail.com>
2023-08-27 17:09:34 +0900

    Update HttpServer

--- Net/HttpServer/TryHttpServer.ps1
+++ Net/HttpServer/TryHttpServer.ps1
@@ -9,7 +9,7 @@
 <CommonParameters> は、サポートしていません。
 
 .EXAMPLE
-TryHttpServer.ps1 localhost 8000
+TryHttpServer.ps1 localhost 8000 -Verbose
 #>
 
 [CmdletBinding()]
@@ -21,10 +21,10 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 #$VerbosePreference = 'Continue'
 #$VerbosePreference = 'SilentlyContinue'
-$psDir = Convert-Path $(Split-Path $MyInvocation.InvocationName -Parent)
+#$psDir = Convert-Path $(Split-Path $MyInvocation.InvocationName -Parent)
 $psName = Split-Path $MyInvocation.InvocationName -Leaf
-$psBaseName = $psName -replace ("\.ps1$", "")
-$timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+#$psBaseName = $psName -replace ("\.ps1$", "")
+#$timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
 
 # ヘルプ
 if (!$hostName -and !$port) {
@@ -38,13 +38,24 @@ Write-Verbose "$psName Start"
 $listener = New-Object Net.HttpListener
 $listener.Prefixes.Add("http://${hostName}:${port}/")
 $listener.Start()
+Write-Verbose "起動 http://${hostName}:${port}/"
 while ($true) {
+  # 待機
   $context = $listener.GetContext()
+
+  # 受信・送信
+  Write-Verbose("受信 Method=$($context.Request.HttpMethod), Url=$($context.Request.RawUrl), Query=$($context.Request.QueryString)")
   $response = $context.Response
   $response.ContentType = "text/plain"
   $content = [System.Text.Encoding]::UTF8.GetBytes("Hello World!")
   $response.OutputStream.Write($content, 0, $content.Length)
   $response.Close()
+
+  # 終了処理
+  # とりあえず簡単な実装にする
+  if ($context.Request.RawUrl -match "exit") {
+    break
+  }
 }
 $listener.Dispose()
 
