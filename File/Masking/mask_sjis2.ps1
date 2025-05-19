99ce856778a160de663d00183a7e38689becb249
kurukurupapa <gootarapapa@gmail.com>
2025-05-19 22:08:33 +0900

    パフォーマンスアップ

--- File/Masking/mask_sjis2.ps1
+++ File/Masking/mask_sjis2.ps1
@@ -23,7 +23,7 @@ Unicode
 出力CSVファイルのパス
 
 .EXAMPLE
-powershell -File .\mask_sjis2.ps1 .\in.csv .\out.csv
+powershell -File .\File\Masking\mask_sjis2.ps1 in.csv out.csv
 .\File\Masking\mask_sjis2.ps1 .\File\Masking\sample_input_sjis.csv .\work\sample_masked_sjis2.csv
 #>
 
@@ -43,95 +43,91 @@ $importEncoding = "Default"
 $exportEncoding = "Default"
 
 # マスキング関数
-function Convert-MaskChar {
-    param($char)
-    switch ($char) {
-        # 半角数字
-        # ASCIIコード：0x30-0x39
-        # [ASCII - Wikipedia](https://ja.wikipedia.org/wiki/ASCII)
-
-        # 全角数字
-        # SJISコード：0x824F-0x8258
-        # Unicodeコード：0xFF10-0xFF19
-        # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
-        # [Unicode 半角・全角形 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html)
-
-        # 半角/全角数字
-        { $_ -match '\d' } { return '9' }
-
-        # ローマ数字
-        # SJISコード：
-        #   0x8754-0x875D（Ⅰ-Ⅹ、NEC特殊文字、Unicode→SJIS変換で使用）
-        #   0xFA4A-0xFA53（Ⅰ-Ⅹ、IBM拡張文字）
-        #   0xFA40-0xFA49（ⅰ-ⅹ、IBM拡張文字、Unicode→SJIS変換で使用）
-        #   0xEEEF-0xEEF8（ⅰ-ⅹ、NEC選定IBM拡張文字）
-        # Unicodeコード：
-        #   0x2160-0x2169（Ⅰ-Ⅹ）、～0x216B（??）
-        #   0x2170-0x2179（ⅰ-ⅹ）、～0x217B（??）
-        # Unicodeカテゴリ：
-        #   0x2150-0x218F（IsNumberForms）
-        # [Windows-31J の重複符号化文字と Unicode](https://www2d.biglobe.ne.jp/~msyk/charcode/cp932/uni2sjis-Windows-31J.html)
-        # [Unicode 数字に準じるもの - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u2150.html)
-        { $_ -match '\p{IsNumberForms}' } { return 'Ⅲ' }
-
-        # 半角英字
-        # ASCIIコード：0x41-0x5A, 0x61-0x7A
-        # [ASCII - Wikipedia](https://ja.wikipedia.org/wiki/ASCII)
-        { $_ -match '[A-Za-z]' } { return 'A' }
-
-        # 全角英字
-        # SJISコード：0x8260-0x8279, 0x8281-0x829A
-        # Unicodeコード：0xFF21-0xFF3A, 0xFF41-0xFF5A
-        # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
-        # [Unicode 半角・全角形 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html)
-        { $_ -match '[Ａ-Ｚａ-ｚ]' } { return 'Ａ' }
-
-        # 半角カタカナ
-        # ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ
-        # SJISコード：0xA6-0xDF
-        # Unicodeコード：0xFF66-0xFF9F
-        # [半角カナ - Wikipedia](https://ja.wikipedia.org/wiki/%E5%8D%8A%E8%A7%92%E3%82%AB%E3%83%8A)
-        # [Unicode 半角・全角形 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html)
-        # [Unicode Character Table - Full List of Unicode Symbols SYMBL](https://symbl.cc/en/unicode-table/#halfwidth-and-fullwidth-forms)
-        { $_ -match '[ｦ-ﾟ]' } { return 'ｱ' }
-
-        # 全角カタカナ
-        # ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ
-        # SJISコード：0x8340-0x8396
-        # Unicodeコード：0x30A1-0x30F6, ～0x30FA（????）
-        # Unicodeカテゴリ：0x30A0-0x30FF（IsKatakana）
-        # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
-        # [Unicode 片仮名 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u30a0.html)
-        { $_ -match '\p{IsKatakana}' } { return 'ア' }
-
-        # 全角ひらがな
-        # ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをん
-        # SJISコード：0x829F-0x82F1
-        # Unicodeコード：0x3041-0x3093, ～0x3096（???）
-        # Unicodeカテゴリ：0x3040-0x309F（IsHiragana）
-        # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
-        # [Unicode 平仮名 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u3040.html)
-        { $_ -match '\p{IsHiragana}' } { return 'あ' }
-
-        # 漢字
-        # SJISコード：0x889F-0xEAA4（亜-熙）、SJISは第1,2水準のみ。
-        # Unicodeコード：0x4E00-0x9FFC（一-?）、拡張/互換漢字は省略。
-        # Unicodeカテゴリ：0x4E00-0x9FFF（IsCJKUnifiedIdeographs）
-        # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
-        # [Unicode CJK統合漢字－全漢字一覧 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/cjku_klist.html)
-        { $_ -match '\p{IsCJKUnifiedIdeographs}' } { return '亜' }
-
-        # 空白・記号などはそのまま
-        default { return $char }
-    }
-}
 function Convert-Field {
     param($text)
     if ([string]::IsNullOrEmpty($text)) { return $text }
-    return ($text.ToCharArray() | ForEach-Object { Convert-MaskChar $_ }) -join ''
+
+    # 半角数字
+    # ASCIIコード：0x30-0x39
+    # [ASCII - Wikipedia](https://ja.wikipedia.org/wiki/ASCII)
+
+    # 全角数字
+    # SJISコード：0x824F-0x8258
+    # Unicodeコード：0xFF10-0xFF19
+    # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
+    # [Unicode 半角・全角形 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html)
+
+    # 半角/全角数字
+    $text = $text -replace '\d', '9'
+
+    # ローマ数字
+    # SJISコード：
+    #   0x8754-0x875D（Ⅰ-Ⅹ、NEC特殊文字、Unicode→SJIS変換で使用）
+    #   0xFA4A-0xFA53（Ⅰ-Ⅹ、IBM拡張文字）
+    #   0xFA40-0xFA49（ⅰ-ⅹ、IBM拡張文字、Unicode→SJIS変換で使用）
+    #   0xEEEF-0xEEF8（ⅰ-ⅹ、NEC選定IBM拡張文字）
+    # Unicodeコード：
+    #   0x2160-0x2169（Ⅰ-Ⅹ）、～0x216B（??）
+    #   0x2170-0x2179（ⅰ-ⅹ）、～0x217B（??）
+    # Unicodeカテゴリ：
+    #   0x2150-0x218F（IsNumberForms）
+    # [Windows-31J の重複符号化文字と Unicode](https://www2d.biglobe.ne.jp/~msyk/charcode/cp932/uni2sjis-Windows-31J.html)
+    # [Unicode 数字に準じるもの - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u2150.html)
+    $text = $text -replace '\p{IsNumberForms}', 'Ⅲ'
+
+    # 半角英字
+    # ASCIIコード：0x41-0x5A, 0x61-0x7A
+    # [ASCII - Wikipedia](https://ja.wikipedia.org/wiki/ASCII)
+    $text = $text -replace '[A-Za-z]', 'A'
+
+    # 全角英字
+    # SJISコード：0x8260-0x8279, 0x8281-0x829A
+    # Unicodeコード：0xFF21-0xFF3A, 0xFF41-0xFF5A
+    # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
+    # [Unicode 半角・全角形 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html)
+    $text = $text -replace '[Ａ-Ｚａ-ｚ]', 'Ａ'
+
+    # 半角カタカナ
+    # ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ
+    # SJISコード：0xA6-0xDF
+    # Unicodeコード：0xFF66-0xFF9F
+    # [半角カナ - Wikipedia](https://ja.wikipedia.org/wiki/%E5%8D%8A%E8%A7%92%E3%82%AB%E3%83%8A)
+    # [Unicode 半角・全角形 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/uff00.html)
+    # [Unicode Character Table - Full List of Unicode Symbols SYMBL](https://symbl.cc/en/unicode-table/#halfwidth-and-fullwidth-forms)
+    $text = $text -replace '[ｦ-ﾟ]', 'ｱ'
+
+    # 全角カタカナ
+    # ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ
+    # SJISコード：0x8340-0x8396
+    # Unicodeコード：0x30A1-0x30F6, ～0x30FA（????）
+    # Unicodeカテゴリ：0x30A0-0x30FF（IsKatakana）
+    # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
+    # [Unicode 片仮名 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u30a0.html)
+    $text = $text -replace '\p{IsKatakana}', 'ア'
+
+    # 全角ひらがな
+    # ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをん
+    # SJISコード：0x829F-0x82F1
+    # Unicodeコード：0x3041-0x3093, ～0x3096（???）
+    # Unicodeカテゴリ：0x3040-0x309F（IsHiragana）
+    # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
+    # [Unicode 平仮名 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u3040.html)
+    $text = $text -replace '\p{IsHiragana}', 'あ'
+
+    # 漢字
+    # SJISコード：0x889F-0xEAA4（亜-熙）、SJISは第1,2水準のみ。
+    # Unicodeコード：0x4E00-0x9FFC（一-?）、拡張/互換漢字は省略。
+    # Unicodeカテゴリ：0x4E00-0x9FFF（IsCJKUnifiedIdeographs）
+    # [JIS X 0208コード表 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/jisx0208.html)
+    # [Unicode CJK統合漢字－全漢字一覧 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/cjku_klist.html)
+    $text = $text -replace '\p{IsCJKUnifiedIdeographs}', '亜'
+    return $text
 }
 
 # CSV読込・マスキング・出力
+$startTime = Get-Date
+Write-Host "START $($startTime.ToString('yyyy/MM/dd HH:mm:ss'))"
+
 $csv = Import-Csv -Path $inputPath -Encoding $importEncoding
 foreach ($row in $csv) {
     # 必要なカラム名に合わせて修正してください
@@ -146,3 +142,6 @@ foreach ($row in $csv) {
 $csv | Export-Csv -Path $outputPath -Encoding $exportEncoding -NoTypeInformation
 
 Write-Host "マスキング済みCSVを $outputPath に出力しました。"
+$endTime = Get-Date
+Write-Host "END $($endTime.ToString('yyyy/MM/dd HH:mm:ss'))"
+Write-Host "処理時間: $(($endTime - $startTime).ToString())"
